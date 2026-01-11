4ecbf55da7c5441f2823bf65f9a1b1b5de5ff6ba
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:47:28 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "api_management_identity_provider_microsofts" {
+  description = "All api_management_identity_provider_microsoft resources"
+  value       = azurerm_api_management_identity_provider_microsoft.api_management_identity_provider_microsofts
+}
+
